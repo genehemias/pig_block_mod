9ac342f6215a643a8de4773a00ac9de75b879ee1
gabriel nehemias <genehemias@gmail.com>
2020-05-11 19:25:42 -0400

    removes old folder structure

--- init.lua
+++
@@ -1,9 +0,0 @@
-minetest.register_node("elinor_mod:pigblock",
-{
-    tiles = {"elinor_mod_pigblock.png"},
-    groups= {
-        snappy = 3,
-        choppy = 3,
-        oddly_breakable_by_hand = 3,
-    }
-})
