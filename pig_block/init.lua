56c3098425bf351f560e28e9a658fa8431bb4d03
gabriel nehemias <genehemias@gmail.com>
2020-05-11 19:20:49 -0400

    fixes folder structure

---
+++ pig_block/init.lua
@@ -0,0 +1,9 @@
+minetest.register_node("elinor_mod:pigblock",
+{
+    tiles = {"elinor_mod_pigblock.png"},
+    groups= {
+        snappy = 3,
+        choppy = 3,
+        oddly_breakable_by_hand = 3,
+    }
+})
